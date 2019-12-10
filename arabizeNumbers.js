ab1b925fd408812ca66b1b8e6ca20dd8853be558
Hassan Elsakndry <hassanramadan03@gmail.com>
2019-12-10 12:41:59 +0200

    done

--- arabizeNumbers.js
+++ arabizeNumbers.js
@@ -1,19 +1,25 @@
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace', 'ArrowRight', 'ArrowLeft', 'Tab'];
+var inputs = document.querySelectorAll('.arabicNumberInput')
 
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Backspace', 'ArrowRight', 'ArrowLeft'];
-input.oninput = () => {
-    input.value = fromEnglishToArabic(input.value)
-    if (input.value.indexOf('.') > -1) input.value = input.value.substring(0, input.value.indexOf('.') + 3)
-};
+inputs.forEach(input => {
+    input.oninput = () => {
+        input.value = fromEnglishToArabic(input.value)
+        if (input.value.indexOf('.') > -1) input.value = input.value.substring(0, input.value.indexOf('.') + 3)
+    };
+    input.onkeyup=event=>onlyNumbers(event)
+    input.onkeydown=event=>onlyNumbers(event)
+})
 
-const onlyNumbers = event => {
+
+const onlyNumbers = (event) => {
     var key = event.key;
-    return (numbers.indexOf(key) > -1) || (input.value.indexOf('.') == -1 && key === '.')
+    return (numbers.indexOf(key) > -1) || (event.target.value.indexOf('.') == -1 && key === '.')
 };
-const viewEnglishNumbers = () => {
-    const element = input.value;
-    const num = fromArabicToEnglish(element)
-    const realNumber = parseFloat(num)
-    alert(realNumber)
+const viewEnglishNumbers = (arabicNumber) => {
+    const num = fromArabicToEnglish(arabicNumber)
+    return parseFloat(num)
+    
 }
 const fromEnglishToArabic = english => english.replace(/[0-9]/g, a => String.fromCharCode(a.charCodeAt(0) + 1584))
 const fromArabicToEnglish = arabic => arabic.replace(/[\u0660-\u0669\u06F0-\u06F9]/g, a => String.fromCharCode((a.charCodeAt(0) & 15) + 48))
+
